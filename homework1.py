55d3a582995845e09c7b23b33c44202a5d185f90
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2024-11-10 19:47:20 +0100

    Add comments
    And fix appending and sorting data when the csv file already has data and is missing data for/until today's date

--- homework1.py
+++ homework1.py
@@ -6,47 +6,80 @@ from datetime import date, timedelta, datetime
 import os
 import time
 
+# start timer for runtime execution
 start = time.time()
 
+# constants
 TODAY = date.today()
 CONTENT_TYPE = 'application/x-www-form-urlencoded'
 DEF_URL = 'https://www.mse.mk/en/stats/symbolhistory/ADIN'
 YEARS_BACK = 10
 
 
+#------------------------CODES---------------------------
+
+def check_number_in_string(s): # for a given string it checks if it contains a number
+    return any(i.isdigit() for i in s)
+
+def get_codes():
+    # get http request
+    response = requests.get(DEF_URL)
+    raw_html = response.text
+    # parse html from request
+    soup = BeautifulSoup(raw_html, "html.parser")
+
+    codes = []
+    # get all the codes from the <select> element and it's <option> elements
+    raw_codes = soup.select_one('#Code').find_all('option')
+    # filter the codes and add them to a list
+    for code in raw_codes:
+        if check_number_in_string(code.text) is not True:
+            codes.append(code.text)
+    return codes
+    
+
 #-----------------------PARSE DATA------------------------------
 
-def get_last_year(input_date):
+# for a given date subtract 364 days
+def get_last_year(input_date): 
     return input_date - timedelta(days=364)
 
-def price_format(price): # from 1,000.00 to 1.000,00
+# transform a string from 1,000.00 to 1.000,00 format
+def price_format(price): 
     if price == '0':
         return price
-
     number = price.replace(',', '.')[:-3]
     decimal = price[-2:]
     return number + ',' + decimal
 
-# parsed data for a given time frame
+# parsed data for a given code and time frame
 def get_parsed_data(code, from_date, to_date):
     code_data = []
-    from_date = from_date.strftime("%m/%d/%Y")
+    # parse data in the right format
+    from_date = from_date.strftime("%m/%d/%Y") 
     to_date = to_date.strftime("%m/%d/%Y")
 
+    # get http request with the given parameters
     response = get_post_request(code, from_date, to_date)
     raw_html = response.text
 
+    # parse the html from the request
     soup = BeautifulSoup(raw_html, "html.parser")
+    # get the table element
     table = soup.select_one('#resultsTable')
     if table is None:
         return None
+    # get all rows <tr> from the table element
     table = table.find_all('tbody')[0].find_all('tr')
 
     for row in table:
+        # for every row get the individual <td> element
         row = row.find_all('td')
         cells = []
+        # get the text of the <td> elements from the current row
         for cell in row:
             cells.append(cell.text)
+        # input the data in the correct format for the current row
         data = {
             "Date": datetime.strptime(cells[0], '%m/%d/%Y').date(),
             "Last trade price": price_format(cells[1]),
@@ -59,12 +92,13 @@ def get_parsed_data(code, from_date, to_date):
             "Total turnover in denars": cells[8]
         }
         code_data.append(data)
-
+    # returns all the rows in the table for the given code and time frame
     return code_data
 
 
 #------------------------POST REQUEST-------------------------
 
+# send a http request with given parameters
 def get_post_request(code, from_date, to_date):
     url = DEF_URL[:-4] + code
 
@@ -78,39 +112,21 @@ def get_post_request(code, from_date, to_date):
 
     response = requests.post(url, headers=header, data=data)
     return response
-
-
-#------------------------CODES---------------------------
-
-def check_number_in_string(s):
-    return any(i.isdigit() for i in s)
-
-def get_codes():
-    response = requests.get(DEF_URL)
-    raw_html = response.text
-    soup = BeautifulSoup(raw_html, "html.parser")
-
-    codes = []
-    raw_codes = soup.select_one('#Code').find_all('option')
-    for code in raw_codes:
-        if check_number_in_string(code.text) is not True:
-            codes.append(code.text)
-    return codes
-
+    
 
 #-----------------------DIRECTORY--------------------------
 
+# check if the database is empty or not
 def check_directory_empty(directory, codes):
     if not os.listdir(directory):
-       # Directory is empty -> create csv for every code
+       # directory is empty -> create csv for every code
        for code in codes:
            filename = code + '.csv'
            path = os.path.join(directory, filename)
-
            with open(path, "w") as csvFile:
                pass
     else:
-        # Directory is not empty -> check if code is missing and create csv
+        # directory is not empty -> check if code is missing and create csv
         files = glob.glob(directory + "/*.csv")
         for code in codes:
             filename = code + '.csv'
@@ -126,22 +142,25 @@ codes = get_codes()
 print("Number of codes:", len(codes))
 
 dir_name = 'database'
+# check if directory exists
 if os.path.isdir(dir_name):
-    # Given directory exists
+    # given directory exists -> check if it's empty
     check_directory_empty(dir_name, codes)
 else:
-    # Given directory doesn't exist -> create dir
+    # given directory doesn't exist -> create directory and check if it's empty
     os.mkdir(dir_name)
     check_directory_empty(dir_name, codes)
 
 # get data for every code
 for code in codes:
     filename = code + '.csv'
+    # check if the file for the given code is empty
     if os.stat(os.path.join(dir_name, filename)).st_size==0: # True if empty
-        # data for the past 10 years
+        # get data for the past 10 years
         i = 0
         to_date = TODAY
         while i<YEARS_BACK:
+            # get from date with a difference of 364 days from the current date
             from_date = get_last_year(to_date)
             data = get_parsed_data(code, from_date, to_date)
 
@@ -150,24 +169,31 @@ for code in codes:
             df = pd.DataFrame(data)
 
             if i==0:
+                # put a header in the csv
                 df.to_csv(os.path.join(dir_name, filename), mode='a', index=False)
             else:
+                # just append the data
                 df.to_csv(os.path.join(dir_name, filename), mode='a', header=False, index=False)
 
+            # get the next set of dates
             to_date = from_date - timedelta(days=1)
             i += 1
-    else: # if csv is not empty get last date + add data from last date until today
+    else: # if csv is not empty -> get last recorded date and append the data from the last recorded date until today
         df = pd.read_csv(os.path.join(dir_name, filename))
-        from_date_file = datetime.strptime(df.iloc[0].tolist()[0], '%Y-%m-%d').date() # last date from csv file
+        # last recorded date from csv file
+        from_date_file = datetime.strptime(df.iloc[0].tolist()[0], '%Y-%m-%d').date() 
 
+        # no new data to fetch
         if from_date_file == TODAY:
             break
 
+        # start from the next day in order not to have duplicates
         from_date_file = from_date_file + timedelta(days=1)
         days_between = from_date_file - TODAY
         to_date = TODAY
         days = abs(days_between.days)
         while True:
+            # fetch data on a "yearly" bases (difference between the dates is more than 364 days)
             if days > 364:
                 from_date = get_last_year(to_date)
                 data = get_parsed_data(code, from_date, to_date)
@@ -180,21 +206,23 @@ for code in codes:
 
                 to_date = from_date - timedelta(days=1)
                 days -= 364
-            else:
+            else: # if the difference is less than a "year" (364 days) no need to itterate, just fetch the data for the given dates
                 data = get_parsed_data(code, from_date_file, to_date)
 
                 # append data to csv
                 filename = code + '.csv'
-                df = pd.DataFrame(data)
-                df.to_csv(os.path.join(dir_name, filename), mode='a', header=False, index=False)
-
-                # sort data
-                df2 = pd.read_csv(os.path.join(dir_name, filename))
-                df2.drop_duplicates()
-                df2.sort_values(by=['Date'], ascending=False, inplace=True)
-                df2.to_csv(os.path.join(dir_name, filename), index=False)
+                # missing data (the one fetched earlier)
+                df1 = pd.DataFrame(data)
+                # current data in file
+                df2 = pd.read_csv(os.path.join(dir_name, filename)) 
+                # join missing and current data in right order
+                frames = [df1, df2]
+                final_df = pd.concat(frames, ignore_index=True)
+                # replace with new data
+                final_df.to_csv(os.path.join(dir_name, filename), index=False) 
                 break
 
+# end runtime execution timer
 end = time.time()
 length = end - start
 
