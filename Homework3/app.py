b12d4af1c3f40da4404769159e987662402977c3
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2024-12-29 12:10:06 +0100

    Add route for the technical analysis

--- Homework3/app.py
+++ Homework3/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_from_directory
 from Homework1.scraper import get_codes
 from datetime import date
 import sqlite3
+from Homework3.chart_indicators import *
 
 TODAY = date.strftime(date.today(), '%Y-%m-%d')
 UPLOAD_FOLDER = '../../Homework1/database/'
@@ -43,6 +44,47 @@ def get_table():
     filename = table_name+'.csv'
 
     return render_template('historicalValues.html', codes=codes, table=table, filename=filename)
+
+@app.route('/marketInsights', methods=['GET'])
+def get_chart():
+    codes = get_codes()
+    table_name = request.args.get('codes')
+    if table_name is None:
+        table_name = 'ADIN'
+
+    fromdate = request.args.get("fromdate")
+    todate = request.args.get("todate")
+
+    option = request.args.get('chart_option')
+
+    plot = None
+
+    match option:
+        case 'SMA':
+            plot = simple_moving_average(fromdate, todate, table_name)
+        case 'EMA':
+            plot = exponential_moving_average(fromdate, todate, table_name)
+        case 'WMA':
+            plot = weighted_moving_average(fromdate, todate, table_name)
+        case 'MACD':
+            plot = macd(fromdate, todate, table_name)
+        case 'CMA':
+            plot = cumulative_moving_average(fromdate, todate, table_name)
+        case 'ADX':
+            plot = adx_indicator(fromdate, todate, table_name)
+        case 'RSI':
+            plot = rsi(fromdate, todate, table_name)
+        case 'CCI':
+            plot = chart_cci(fromdate, todate, table_name)
+        case 'MFI':
+            plot = money_fow_index(fromdate, todate, table_name)
+        case 'RIBBON MA':
+            plot = ribbon_moving_averages(fromdate, todate, table_name)
+        case 'STOCHASTIC':
+            plot = stochastic_oscillator(fromdate, todate, table_name)
+
+
+    return render_template('technicalAnalysis.html', codes=codes, graphJSON=plot)
 
 if __name__ == '__main__':
     app.run(debug=True)
