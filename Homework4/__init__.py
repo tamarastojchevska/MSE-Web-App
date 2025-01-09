31cd3a12338200aa5a2295e0f9cc0f5e9e2d33ae
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2025-01-09 20:18:28 +0100

    Add Blueprints to the main app

---
+++ Homework4/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from Homework4.DataScraperService.api import data_scraper_api
+from Homework4.RenderTemplatesService import routes as templates_api
+from Homework4.SQLiteService.api import sqlite_api
+from Homework4.TechnicalAnalysisService.api import plots_api, calculations_api
+from Homework4.TickersService.api import tickers_api
+
+
+app = Flask(__name__)
+app.secret_key = 'SECRET_KEY'
+
+app.register_blueprint(data_scraper_api.data_bp)
+app.register_blueprint(templates_api.templates_bp)
+app.register_blueprint(sqlite_api.sqlite_bp)
+app.register_blueprint(plots_api.plot_bp)
+app.register_blueprint(calculations_api.calculations_bp)
+app.register_blueprint(tickers_api.tickers_bp)
+
