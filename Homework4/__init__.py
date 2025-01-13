45698af2490fb84771bb4a2dd6d40cf6245b7fd6
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2025-01-13 23:22:11 +0100

    Add csv Blueprint to main app

--- Homework4/__init__.py
+++ Homework4/__init__.py
@@ -5,7 +5,7 @@ from Homework4.RenderTemplatesService import routes as templates_api
 from Homework4.SQLiteService.api import sqlite_api
 from Homework4.TechnicalAnalysisService.api import plots_api, calculations_api
 from Homework4.TickersService.api import tickers_api
-
+from Homework4.CSVdataService.api import csv_data_api
 
 app = Flask(__name__)
 app.secret_key = 'SECRET_KEY'
@@ -16,4 +16,4 @@ app.register_blueprint(sqlite_api.sqlite_bp)
 app.register_blueprint(plots_api.plot_bp)
 app.register_blueprint(calculations_api.calculations_bp)
 app.register_blueprint(tickers_api.tickers_bp)
-
+app.register_blueprint(csv_data_api.csv_data_bp)
