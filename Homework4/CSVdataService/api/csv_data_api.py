e6a445a3dac6dfbb4bb23c6902fed71a5453db99
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2025-01-13 23:29:01 +0100

    Fix when filename is empty

--- Homework4/CSVdataService/api/csv_data_api.py
+++ Homework4/CSVdataService/api/csv_data_api.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import send_from_directory, request, jsonify
 from Homework4.CSVdataService import csv_data_bp
 
@@ -7,8 +9,6 @@ DOWNLOAD_FOLDER = './csv_database'
 @csv_data_bp.route('/csv/download', methods = ['GET'])
 def csv_data_download():
     filename = request.args.get('filename')
-    if filename is not None:
-        return send_from_directory(DOWNLOAD_FOLDER, filename, as_attachment=True)
-    else:
-        jsonify('File not found')
-
+    if filename == "":
+       filename = os.listdir(DOWNLOAD_FOLDER)[0]
+    return send_from_directory(DOWNLOAD_FOLDER, filename, as_attachment=True)
