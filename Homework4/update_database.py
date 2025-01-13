967c182544328c12996d10223582f90e2b549577
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2025-01-13 23:16:33 +0100

    Add function for csv database update

--- Homework4/update_database.py
+++ Homework4/update_database.py
@@ -1,13 +1,14 @@
 import requests
+from Homework4.CSVdataService.model.data_to_csv import CSVdata
 from Homework4.SQLiteService.model.sqlite_database import SqliteDatabase
 
 
 tickers_url = 'http://127.0.0.1:5000/tickers'
+tickers = requests.get(tickers_url).json()
 
-def update_database():
-    tickers = requests.get(tickers_url).json()
-
+def update_sqlite_database():
     for ticker in tickers:
-        print(ticker)
         SqliteDatabase.upload_ticker_data_to_database(ticker)
 
+def update_csv_database(directory):
+    CSVdata.check_directory_exist(directory, tickers)
