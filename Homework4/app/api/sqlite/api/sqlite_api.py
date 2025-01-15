2c13f7cb2eed7617f52f0c42b2b095622099d7e8
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2025-01-15 22:21:39 +0100

    Remove sqlite data upload route

--- Homework4/app/api/sqlite/api/sqlite_api.py
+++ Homework4/app/api/sqlite/api/sqlite_api.py
@@ -18,10 +18,3 @@ def get_sqlite_ticker_data_dates(ticker, from_date, to_date):
     except sqlite3.Error as e:
         print('Error while getting ticker data from database with date parameters: ', e)
 
-@sqlite_bp.route('/tickers/sqlite/upload/<string:ticker>', methods=['POST'])
-def upload_sqlite_ticker_data(ticker):
-    try:
-        sqlite_database.upload_ticker_data_to_database(ticker)
-    except sqlite3.Error as e:
-        print('Error while uploading ticker data to database: ', e)
-
