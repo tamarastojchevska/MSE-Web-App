ef2ad1e51ad31cad328943d6d7dd528d82cd0813
stojchevska <138624193+tamarastojchevska@users.noreply.github.com>
2025-01-16 21:33:10 +0100

    Add scheduler for database update

--- Homework4/app/update_database.py
+++ Homework4/app/update_database.py
@@ -2,6 +2,9 @@ import requests
 from Homework4.app.service.csv.model import data_to_csv
 from Homework4.app.service.sqlite.model import sqlite_database
 from Homework4.app.service import api_urls
+import schedule
+import time
+
 
 tickers = requests.get(api_urls.tickers_url).json()
 directory = './csv_database'
@@ -13,6 +16,8 @@ def update_sqlite_database():
 def update_csv_database():
     data_to_csv.check_directory_exist(directory, tickers)
 
+schedule.every().day.at("00:00").do(update_sqlite_database).do(update_csv_database)
 
-update_sqlite_database()
-update_csv_database()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
